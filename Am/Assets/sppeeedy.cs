438924c890141e390bf13563d1980d149f150aef
Midnitepanda <Midnitepanda398@gmail.com>
2023-07-28 17:44:05 -0400

    gay ass migit porn

--- Am/Assets/sppeeedy.cs
+++ Am/Assets/sppeeedy.cs
@@ -17,7 +17,7 @@ public class sppeeedy : MonoBehaviour
     {
         if (Input.GetKeyDown (KeyCode.Space))
         {
-            myRigidbody.velocity = Vector2.up * 10;
+            myRigidbody.velocity = Vector2.up * 400;
         } 
 
 
