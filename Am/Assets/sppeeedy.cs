c327010b32f3bfea28be7ba5e4d13c18ba5e2a29
Midnitepanda <Midnitepanda398@gmail.com>
2023-07-29 17:49:32 -0400

    fuck me in the ass

---
+++ Am/Assets/sppeeedy.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        // Get the SpriteRenderer component from the game object
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            pos.y += speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            pos.y -= speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            pos.x += speed * Time.deltaTime;
+            // Do not flip the sprite (face right)
+            spriteRenderer.flipX = true;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            pos.x -= speed * Time.deltaTime;
+            // Flip the sprite to face left
+            spriteRenderer.flipX = false;
+        }
+
+        transform.position = pos;
+    }
+}
