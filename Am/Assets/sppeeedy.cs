ae911d8a4114f66a28ad731ce988954d20121f90
kingofq <140895288+kingofq@users.noreply.github.com>
2023-07-30 20:33:48 +0200

    Updates
    -Backdrop upgraded
    -jump mechanism improved
    -ground extended

--- Am/Assets/sppeeedy.cs
+++ Am/Assets/sppeeedy.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 
+
 public class Movement : MonoBehaviour
 {
     public float speed = 100f;
     public bool grounded;
     public int counter = 0;
+    public float max_horizontal = 1000f;
     private SpriteRenderer spriteRenderer;
     private Rigidbody2D rb2D;
 
@@ -34,6 +36,7 @@ public class Movement : MonoBehaviour
     void Update()
     {
         Vector3 pos = transform.position;
+        
 
         if (Input.GetKey(KeyCode.UpArrow) && grounded == true)
         {
@@ -46,21 +49,31 @@ public class Movement : MonoBehaviour
         {
             if (counter < 500)
             {
-                rb2D.AddForce(Vector2.up * 1, ForceMode2D.Impulse);
+                rb2D.AddForce(Vector2.up * 1.4f, ForceMode2D.Impulse);
                 counter += 1;
             }
         } 
     
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            pos.x += speed * Time.deltaTime;
+            
+            // pos.x += speed * Time.deltaTime;
+                if (rb2D.velocity.x < max_horizontal)
+                {
+                    rb2D.velocity = new Vector3(rb2D.velocity.x + 5f, rb2D.velocity.y);
+                }
+            
             // Do not flip the sprite (face right)
             spriteRenderer.flipX = true;
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            pos.x -= speed * Time.deltaTime;
+            if ((rb2D.velocity.x > -1*max_horizontal))
+            {
+                rb2D.velocity = new Vector3(rb2D.velocity.x - 5f, rb2D.velocity.y);
+                }
+            //pos.x -= speed * Time.deltaTime;
             // Flip the sprite to face left
             spriteRenderer.flipX = false;
         }
