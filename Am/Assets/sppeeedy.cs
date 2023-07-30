45f2827dbc0ae4919c52cbdc9707b19a5d31b923
kingofq <140895288+kingofq@users.noreply.github.com>
2023-07-30 18:04:42 +0200

    updated speed and jump

--- Am/Assets/sppeeedy.cs
+++ Am/Assets/sppeeedy.cs
@@ -2,25 +2,55 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 10f;
-
+    public float speed = 100f;
+    public bool grounded;
+    public int counter = 0;
     private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb2D;
 
     void Start()
     {
         // Get the SpriteRenderer component from the game object
         spriteRenderer = GetComponent<SpriteRenderer>();
+        rb2D = GetComponent<Rigidbody2D>();
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ground")
+        {
+            grounded = true;
+        }
+    }
+
+    //void OnCollisionExit2D(Collision2D collision)
+    //{
+    // if (collision.gameObject.tag == "ground")
+    //{
+    //grounded = false;
+    // }
+    // }
 
     void Update()
     {
         Vector3 pos = transform.position;
 
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) && grounded == true)
         {
-            pos.y += speed * Time.deltaTime;
-        }
+            //pos.y += speed * Time.deltaTime;
 
+            grounded = false;
+            counter = 0;
+        }
+        if (grounded == false)
+        {
+            if (counter < 500)
+            {
+                rb2D.AddForce(Vector2.up * 1, ForceMode2D.Impulse);
+                counter += 1;
+            }
+        } 
+    
         if (Input.GetKey(KeyCode.RightArrow))
         {
             pos.x += speed * Time.deltaTime;
